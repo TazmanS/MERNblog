20be5fe9de6d8b713277ca28416727728970f41e
TazmanS <33206020+TazmanS@users.noreply.github.com>
2020-03-29 13:38:07 +0300

    User entrance

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -7,7 +7,9 @@ router.post('/add', async (req, res) => {
     try{
         await User.findOne({login: req.body.login}).then((findEl) => {
             if(findEl){
-                res.status(200).json("Login not free!!!")
+                res.status(200).json({
+                    data: "Login not free!!!"
+                })
                 throw Error("Login not free!!!")
             }
         })
@@ -26,6 +28,36 @@ router.post('/add', async (req, res) => {
         })
         
     }catch (e){
+        console.log(e)
+    }
+})
+
+router.post('/entrancelocalstorage', async (req, res) => {
+    try{
+        await User.find({id: req.body.userId}).then(one => {
+            res.status(200).json(one)
+        })
+    } catch(e){
+        console.log(e)
+    }
+})
+
+router.post('/entrance', async (req, res) => {
+    try{
+        await User.find({
+            login: req.body.login, 
+            password: req.body.password
+        }).then(one => {
+            if(one[0]){
+                res.status(200).json(one)
+            } else{
+                res.status(201).json("User not found")
+                console.log("User not found")
+            }
+        }) 
+
+
+    } catch(e){
         console.log(e)
     }
 })
